d45565a962a74c08ff906a4d28dfaa16f3e44afb
Nick Rook <rooknj@outlook.com>
2019-02-20 13:34:08 -0500

    added password script

--- setup_funcs.py
+++ setup_funcs.py
@@ -1,4 +1,4 @@
-import os
+import os,subprocess,crypt,random,sys
 
 def setup_hostname(hostname):
   if hostname == "":
@@ -6,3 +6,18 @@ def setup_hostname(hostname):
   os.system("raspi-config nonint do_hostname " + hostname)
   os.system("hostname -b " + hostname)
   os.system("systemctl restart avahi-daemon")
+
+def setup_password(password):
+  login = 'pi'
+  if password == "":
+    password = "PrysmaPi"
+
+  ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ./"
+  salt = ''.join(random.choice(ALPHABET) for i in range(16))
+
+  shadow_password = crypt.crypt(password,'$6$'+salt+'$')
+
+  r = subprocess.call(('usermod', '-p', shadow_password, login))
+
+  if r != 0:
+      print('Error changing password for ' + login)
