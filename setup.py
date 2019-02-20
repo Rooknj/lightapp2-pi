11cd949592a45156c9dd1e16f56a0647a441b59b
Nick Rook <rooknj@outlook.com>
2019-02-20 13:23:22 -0500

    added hostname setup

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import setup_funcs
 
 if os.getuid():
   sys.exit('You need root access to install!')
@@ -12,14 +13,14 @@ print("##### Prysmalight Intial Setup  #####")
 print("#####################################")
 print()
 print()
-hostname = input("Would you like to change the hostname? [default: prysma]: ")
+hostname = input("Would you like to change the hostname? [default: prysma2]: ")
 os.system('clear')
 print()
 print()
 install_ans = input("Are you ready to commit changes to the system? [y/N]: ")
 
 if(install_ans.lower() == 'y'):
-	print("INstaalling")
+	setup_funcs.setup_hostname(hostname)
 else:
 	print()
 	print()
---
+++ setup_funcs.py
@@ -0,0 +1,8 @@
+import os
+
+def setup_hostname(hostname):
+  if hostname == "":
+    hostname = "prysma2"
+  os.system("raspi-config nonint do_hostname " + hostname)
+  os.system("hostname -b" + hostname)
+  os.system("systemctl restart avahi-daemon")
